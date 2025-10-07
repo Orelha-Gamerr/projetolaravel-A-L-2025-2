881506ada53f4f2dfc72e0b3799523f52dca1ea5
Orelha-Gamerr <angelolucietto@gmail.com>
2025-10-07 08:58:39 -0300

    Ajusting on the Categoria seeder and Factory

--- database/factories/CategoriaServicoFactory.php
+++ database/factories/CategoriaServicoFactory.php
@@ -4,12 +4,28 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CategoriaServico>
+ */
 class CategoriaServicoFactory extends Factory
 {
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
     public function definition(): array
     {
         return [
-            'nome' => $this->faker->word(),
+            'nome' => $this->faker->unique()->randomElement([
+                'MOTOR',
+                'CÂMBIO',
+                'ILUMINAÇÃO',
+                'ESCAPAMENTO',
+                'SUSPENSÃO',
+                'FREIOS',
+                'DIREÇÃO',
+            ]),
         ];
     }
 }
--- database/seeders/CategoriaServicoSeeder.php
+++ database/seeders/CategoriaServicoSeeder.php
@@ -2,25 +2,17 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\CategoriaServico;
 
 class CategoriaServicoSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     */
     public function run(): void
     {
-        $categorias = [
-            'MOTOR',
-            'CÂMBIO',
-            'ILUMINAÇÃO',
-            'ESCAPAMENTO',
-            'SUSPENSÃO',
-            'FREIOS',
-            'DIREÇÃO',
-        ];
-
-        foreach ($categorias as $categoria) {
-            CategoriaServico::firstOrCreate(['nome' => $categoria]);
-        }
+        CategoriaServico::factory()->count(7)->create();
     }
 }
