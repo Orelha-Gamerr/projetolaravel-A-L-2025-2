f44502b0aedae5ba9e947021f3ceb55b551cd03f
Orelha-Gamerr <angelolucietto@gmail.com>
2025-10-24 08:58:28 -0300

    Remove categoria_id from ServicoFactory to streamline service creation

--- database/factories/ServicoFactory.php
+++ database/factories/ServicoFactory.php
@@ -15,7 +15,6 @@ class ServicoFactory extends Factory
         return [
             'cliente_id' => Cliente::inRandomOrder()->first()->id ?? Cliente::factory(),
             'carro_id' => Carro::inRandomOrder()->first()->id ?? Carro::factory(),
-            'categoria_id' => CategoriaServico::inRandomOrder()->first()->id ?? CategoriaServico::factory(),
             'descricao' => $this->faker->text(200),
             'data_servico' => $this->faker->dateTimeBetween('2021-01-01', '2025-12-31')->format('Y-m-d'),
             'valor' => $this->faker->randomFloat(2, 50, 1000),
